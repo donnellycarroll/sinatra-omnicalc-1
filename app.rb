f4776404731a340f45922e53ea87e0bda6032e9c
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-10-22 23:22:13 +0000

    added squares

--- app.rb
+++ app.rb
@@ -7,3 +7,15 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/howdy")do
+  erb(:hello)
+end
+
+get("/square/new")do
+  erb(:new_square_calc)
+end
+
+get("/square/results")do
+  erb(:square_results)
+end
